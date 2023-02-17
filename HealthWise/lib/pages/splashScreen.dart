90236233a03004e507c6ca905124a8ef71d9a4b5
Abhishek <abhitiwari3010@gmail.com>
2023-02-18 01:01:55 +0530

    created splash screen

---
+++ HealthWise/lib/pages/splashScreen.dart
@@ -0,0 +1,32 @@
+import 'dart:async';
+
+import 'package:Healthwise/helpers/frontEnd.dart';
+import 'package:Healthwise/pages/home.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+        Duration(seconds: 3),
+        () => Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => Home())));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: application_name,
+    );
+  }
+}
