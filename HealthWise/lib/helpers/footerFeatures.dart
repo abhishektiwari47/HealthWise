d05523e089c9284fe6c006de8cffc0b7f294a88f
Abhishek <abhitiwari3010@gmail.com>
2023-03-20 22:01:39 +0530

    footer features code for water and profile

---
+++ HealthWise/lib/helpers/footerFeatures.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+
+class FooterFeatures extends StatefulWidget {
+  const FooterFeatures({
+    super.key,
+  });
+
+  @override
+  State<FooterFeatures> createState() => _FooterFeaturesState();
+}
+
+class _FooterFeaturesState extends State<FooterFeatures> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Spacer(),
+        Container(
+          height: 50,
+          width: 50,
+          clipBehavior: Clip.hardEdge,
+          decoration: BoxDecoration(
+              // color: Colors.blue,
+              // borderRadius: BorderRadius.all(
+              //   Radius.circular(30),
+              // ),
+              ),
+          child: Center(
+            child: Material(
+              clipBehavior: Clip.hardEdge,
+              borderRadius: BorderRadius.all(Radius.circular(50)),
+              color: Colors.pink,
+              child: IconButton(
+                  iconSize: 30,
+                  splashRadius: 120,
+                  splashColor: Color.fromARGB(255, 255, 223, 223),
+                  // tooltip: 'Opening Camera',
+                  icon: Icon(
+                    Icons.water_drop,
+                    color: Color.fromARGB(255, 255, 254, 254),
+                  ),
+                  onPressed: () {}),
+            ),
+          ),
+        ),
+        Spacer(),
+        Spacer(),
+        Container(
+          height: 50,
+          width: 50,
+          clipBehavior: Clip.hardEdge,
+          decoration: BoxDecoration(
+              // color: Colors.blue,
+              // borderRadius: BorderRadius.all(
+              //   Radius.circular(30),
+              // ),
+              ),
+          child: Center(
+            child: Material(
+              clipBehavior: Clip.hardEdge,
+              borderRadius: BorderRadius.all(Radius.circular(50)),
+              color: Colors.pink,
+              child: IconButton(
+                  iconSize: 30,
+                  splashRadius: 120,
+                  splashColor: Color.fromARGB(255, 255, 223, 223),
+                  // tooltip: 'Opening Camera',
+                  icon: Icon(
+                    Icons.person,
+                    color: Color.fromARGB(255, 255, 254, 254),
+                  ),
+                  onPressed: () {}),
+            ),
+          ),
+        ),
+        Spacer()
+      ],
+    );
+  }
+}
+
+
+
+
+
+// Calculating the approximate calories burned per step of a person according to their BMI involves a few steps:
+
+// Calculate the person's basal metabolic rate (BMR). The BMR is the amount of energy (in calories) that a person's body burns at rest to maintain basic bodily functions such as breathing and circulating blood. The most widely used formula to calculate BMR is the Harris-Benedict equation:
+// For men: BMR = 88.362 + (13.397 x weight in kg) + (4.799 x height in cm) - (5.677 x age in years)
+// For women: BMR = 447.593 + (9.247 x weight in kg) + (3.098 x height in cm) - (4.330 x age in years)
+
+// Multiply the person's BMR by their activity factor to estimate their total daily energy expenditure (TDEE). The activity factor is a number that reflects how active a person is during the day. For example, a sedentary person (e.g. desk job, no exercise) would have an activity factor of 1.2, while a very active person (e.g. construction worker, intense exercise) would have an activity factor of 1.9. The TDEE is an estimate of how many calories a person burns in a day, taking into account their basal metabolic rate and their level of physical activity.
+// TDEE = BMR x activity factor
+
+// Calculate the person's average stride length. This can be estimated by dividing the person's height in centimeters by 2.5.
+// Stride length = height in cm / 2.5
+
+// Divide the person's TDEE by the number of steps they take in a day to estimate the number of calories burned per step.
+// Calories burned per step = TDEE / number of steps
+
+// Note that these calculations are only an estimate and may not be accurate for every individual. Additionally, there are many other factors that can affect how many calories a person burns per step, such as their walking speed, terrain, and other physical characteristics.
