a321d76e3eefcd6cec49b1cdda76bca31f20fa71
Abhishek <abhitiwari3010@gmail.com>
2023-03-20 15:59:29 +0530

    Created Addtional Features Row

---
+++ HealthWise/lib/helpers/additionalFeatures.dart
@@ -0,0 +1,59 @@
+import 'package:Healthwise/helpers/dataVariables.dart';
+import 'package:flutter/material.dart';
+
+class AdditionalFeatures extends StatefulWidget {
+  var emoji;
+
+  var bottomName;
+
+  AdditionalFeatures({
+    super.key,
+    required this.emoji,
+    required this.bottomName,
+  });
+
+  @override
+  State<AdditionalFeatures> createState() => _AdditionalFeaturesState();
+}
+
+class _AdditionalFeaturesState extends State<AdditionalFeatures> {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => {},
+      child: Container(
+        height: 90,
+        width: 90,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Color.fromARGB(255, 253, 209, 209),
+              blurRadius: 3.0, // soften the shadow
+              spreadRadius: 3.0, //extend the shadow
+            )
+          ],
+        ),
+        child: Center(
+            child: Column(
+          children: [
+            Spacer(),
+            Text(
+              widget.emoji,
+              style: TextStyle(color: textColor, fontSize: 30),
+            ),
+            Spacer(),
+            Text(
+              widget.bottomName,
+              textAlign: TextAlign.left,
+              style: TextStyle(
+                  color: Color.fromARGB(255, 255, 159, 16), fontSize: 15),
+            ),
+            Spacer()
+          ],
+        )),
+      ),
+    );
+  }
+}
