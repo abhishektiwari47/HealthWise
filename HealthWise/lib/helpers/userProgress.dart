28ce263f357640e40889db5ad4c0c6ea548a7375
Abhishek <abhitiwari3010@gmail.com>
2023-03-21 15:01:51 +0530

    replaced boxDecoration from mainWhiteBoxDecoration

--- HealthWise/lib/helpers/userProgress.dart
+++ HealthWise/lib/helpers/userProgress.dart
@@ -1,3 +1,4 @@
+import 'package:Healthwise/helpers/frontEnd.dart';
 import 'package:flutter/material.dart';
 
 class UserProgress extends StatefulWidget {
@@ -13,19 +14,6 @@ class _UserProgressState extends State<UserProgress> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 50,
-      width: 310,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.all(Radius.circular(10)),
-        color: Color.fromARGB(255, 255, 255, 255),
-        boxShadow: [
-          BoxShadow(
-            color: Color.fromARGB(255, 253, 209, 209),
-            blurRadius: 5.0, // soften the shadow
-            spreadRadius: 1.0, //extend the shadow
-          )
-        ],
-      ),
-    );
+        height: 70, width: 310, decoration: mainWhiteBoxDecoration);
   }
 }
