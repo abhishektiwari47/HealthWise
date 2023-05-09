0d06ad039af4bb2dd18c197fee31e06c0304bed9
Abhishek <abhitiwari3010@gmail.com>
2023-05-09 19:07:28 +0530

    added Color and Decoration Variables

--- HealthWise/lib/helpers/dataVariables.dart
+++ HealthWise/lib/helpers/dataVariables.dart
@@ -22,6 +22,40 @@ String itemName = '';
 
 List<String> CardFruitList = ["Apple", "Banana", "Carrot"];
 
-final Color bgColor = Color.fromARGB(255, 253, 239, 238);
+//Style Variables
+const Color bgColor = Color.fromARGB(255, 238, 253, 241);
+const Color fadedBgColor = Color.fromARGB(255, 248, 255, 251);
+const Color textColorGreen = Color.fromARGB(255, 68, 195, 81);
+const Color textColorGrey = Color.fromARGB(255, 80, 80, 80);
+const Color textColorYellow = Color.fromARGB(255, 255, 159, 16);
+const Color textColorGreyFaded = Color.fromARGB(255, 179, 174, 174);
+const Color iconColor = Colors.green;
+const Color splashColor = Color.fromARGB(255, 223, 255, 224);
+const Color whiteColor = Colors.white;
+const Color blackColor = Colors.black;
+const Color positivePointColor = Color.fromARGB(255, 224, 255, 228);
+const Color negetivePointColor = Color.fromARGB(255, 255, 240, 240);
+const Color specialTileColor = Color.fromARGB(255, 146, 60, 74);
+const Color whiteBoxGreenShadow = Color.fromARGB(255, 209, 253, 215);
+const Color circularProgressIndicator = Color.fromARGB(255, 200, 255, 210);
 
-final Color textColor = Color.fromARGB(255, 253, 126, 153);
+//Shadow Variable
+List<BoxShadow> mainShadow = [
+  const BoxShadow(
+    color: whiteBoxGreenShadow,
+    blurRadius: 2.0, // soften the shadow
+    spreadRadius: 1.0, //extend the shadow
+  )
+];
+
+//Decoration
+Decoration mainWhiteBoxDecoration = BoxDecoration(
+  borderRadius: const BorderRadius.all(Radius.circular(10)),
+  color: whiteColor,
+  boxShadow: mainShadow,
+);
+
+Decoration fadedBoxDecoration = const BoxDecoration(
+  borderRadius: BorderRadius.all(Radius.circular(2)),
+  color: fadedBgColor,
+);
