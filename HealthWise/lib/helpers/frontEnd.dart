fbd8a7c6e7d6dc1678b9c835cec05604e1140305
Abhishek <abhitiwari3010@gmail.com>
2023-05-09 19:06:32 +0530

    moved primary color, mainshadow, mainwhiteboxdec

--- HealthWise/lib/helpers/frontEnd.dart
+++ HealthWise/lib/helpers/frontEnd.dart
@@ -1,31 +1,12 @@
+import 'package:Healthwise/helpers/dataVariables.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-
-//Colour Variables
-final primary_color = Color.fromARGB(255, 241, 148, 148);
 
 //Size Variables
 final general_padding = const EdgeInsets.all(8.0);
 
 //Text Variables
 final application_name = Text("HealthWise");
-
-//Shadow Variable
-List<BoxShadow> mainShadow = [
-  BoxShadow(
-    color: Color.fromARGB(255, 253, 209, 209),
-    blurRadius: 3.0, // soften the shadow
-    spreadRadius: 3.0, //extend the shadow
-  )
-];
-
-//Decoration
-Decoration mainWhiteBoxDecoration = BoxDecoration(
-  borderRadius: BorderRadius.all(Radius.circular(10)),
-  //
-  color: Color.fromARGB(255, 255, 255, 255),
-  boxShadow: mainShadow,
-);
 
 //NetworkImageWithLoadingIndicator
 
@@ -49,7 +30,7 @@ class ImageFromNetwork extends StatelessWidget {
           // child: Icon(Icons.image),
 
           child: CircularProgressIndicator(
-            color: Color.fromARGB(255, 255, 200, 200),
+            color: circularProgressIndicator,
             value: loadingProgress.expectedTotalBytes != null
                 ? loadingProgress.cumulativeBytesLoaded /
                     loadingProgress.expectedTotalBytes!
